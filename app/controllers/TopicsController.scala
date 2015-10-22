bfed226be552d2ad8e320b84513d4914e77c3705
vincent178 <vincent.007.cn@gmail.com>
2015-10-22 21:57:08 +0800

    add pretty time helper

--- app/controllers/TopicsController.scala
+++ app/controllers/TopicsController.scala
@@ -11,8 +11,8 @@ object TopicsController extends Controller {
   def index = Action {
 
     val topics = Seq(
-      Topic("[广州][2015年10月28日 19:00] GZRUBY 第 27 次聚会 [Post RubyConf 专场]", "allenfantasy", "线下活动", new DateTime("2004-12-13T21:39:45.618-08:00")),
-      Topic("欢迎 3 位新的 Ruby China 社区管理员", " huacnlee", "公告", new DateTime("2004-12-13T21:39:45.618-08:00")),
+      Topic("[广州][2015年10月28日 19:00] GZRUBY 第 27 次聚会 [Post RubyConf 专场]", "allenfantasy", "线下活动", new DateTime("2015-09-13T21:39:45.618-08:00")),
+      Topic("欢迎 3 位新的 Ruby China 社区管理员", " huacnlee", "公告", new DateTime("2015-10-13T21:39:45.618-08:00")),
       Topic("RubyConf China 2015 大会照片收集贴", "lgn21st", "RubyConf", new DateTime("2004-12-13T21:39:45.618-08:00"))
     )
     Ok(views.html.topics.index(topics))
--- app/helpers/PrettyTime.scala
+++ app/helpers/PrettyTime.scala
@@ -1,23 +1,38 @@
 package helpers
 
-import org.joda.time.{Period, DateTime}
-import org.joda.time.format.{PeriodFormatterBuilder, PeriodFormatter}
+import org.joda.time.{DateTime, Duration}
+import play.api.Logger
 
 object PrettyTime {
+  val SECOND = 1
+  val MINUTE = SECOND * 60
+  val HOUR = MINUTE * 60
+  val DAY = HOUR * 24
+  val MONTH = DAY * 30
+  val YEAR = MONTH * 12
 
   def apply(pastDateTime: DateTime, currentDateTime: DateTime = new DateTime()) = {
 
-    val formatter: PeriodFormatter = new PeriodFormatterBuilder()
-      .appendSeconds().appendPrefix(" seconds ago\n")
-      .appendMinutes().appendPrefix(" minutes ago\n")
-      .appendHours().appendPrefix(" hours ago\n")
-      .appendWeeks().appendPrefix(" days ago\n")
-      .appendMonths().appendPrefix(" weeks ago\n")
-      .appendYears().appendPrefix(" years ago\n")
-      .printZeroNever()
-      .toFormatter
+    val duration = new Duration(pastDateTime, currentDateTime)
+    val intevals = duration.getStandardSeconds
 
-    val period = new Period(pastDateTime, currentDateTime)
-    formatter.print(period)
+    Logger.debug("TIME INTEVALS: " + intevals.toString)
+
+    intevals match {
+      case lessThanSecond if lessThanSecond <= SECOND
+        =>  "刚刚"
+      case lessThanMinute if lessThanMinute <= MINUTE
+        => (lessThanMinute / SECOND).toString + " 秒以前"
+      case lessThanHour if lessThanHour <= HOUR
+        => (lessThanHour / MINUTE).toString + " 分钟以前"
+      case lessThanDay if lessThanDay <= DAY
+        => (lessThanDay / HOUR).toString + " 小时以前"
+      case lessThanMonth if lessThanMonth <= MONTH
+        => (lessThanMonth / DAY).toString + " 天以前"
+      case lessThanYear if lessThanYear <= YEAR
+        => (lessThanYear / MONTH).toString + " 月以前"
+      case biggerThanYear if biggerThanYear > YEAR
+        => (biggerThanYear / YEAR).toString + " 年以前"
+    }
   }
 }
