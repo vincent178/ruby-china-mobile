6ec8422a13b08bf1bc0fabd25eb07bf0c0a5b656
vincent178 <vincent.007.cn@gmail.com>
2015-10-30 09:38:58 +0800

    retrieve access_token after user authorize

--- app/controllers/OAuthConsumers.scala
+++ app/controllers/OAuthConsumers.scala
@@ -1,22 +1,33 @@
 package controllers
 
+import play.api.libs.ws._
+import scala.concurrent.Future
 import play.api.mvc._
 import play.Play
+import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object OAuthConsumers extends Controller {
 
-  def callback = Action { implicit request =>
+  def callback = Action.async { implicit request =>
 
     // POST https://github.com/login/oauth/access_token
 
     // client_id, client_secret, code (get from request)
     // redirect_uri, state
 
+
     val code = request.getQueryString("code")
-    val clientId = Play.application().configuration().getString("github.clientId")
+    val clientId = Play.application.configuration.getString("github.clientId")
     val clientSecret = Play.application.configuration.getString("github.clientSecret")
 
-    Ok(views.html.home.index())
+    WS.url("https://github.com/login/oauth/access_token")
+      .withHeaders("Content-Type" -> "application/json")
+      .withQueryString("code" -> code.getOrElse(""), "client_id" -> clientId, "client_secret" -> clientSecret).post("").map { response =>
+
+      // response.body = "access_token=76d10c2c197a13c31655485e39d4cc11235201cd&scope=&token_type=bearer"
+      Ok(response.json.toString)
+    }
   }
 
 }
