db1c6b3bc78deb136be0d4d086215c5aaf46005f
vincent178 <vincent.007.cn@gmail.com>
2015-11-03 06:50:21 +0800

    refactor oauth access_token

---
+++ app/helpers/OAuth.scala
@@ -0,0 +1,26 @@
+package helpers
+
+import play.api.{Logger, Play}
+import play.api.libs.ws.WS
+
+class OAuth {
+
+  // need pass in code
+  def githubAccessToken(code: String) = {
+    val clientId = Play.application.configuration.getString("github.clientId")
+    val clientSecret = Play.application.configuration.getString("github.clientSecret")
+    val postBody = Map("code" -> Seq(code), "client_id" -> Seq(clientId), "client_secret" -> Seq(clientSecret))
+
+    WS.url("https://github.com/login/oauth/access_token")
+      .post(postBody).map { response =>
+
+      Logger.info("Response status: " + response.status)
+      Logger.info("Response body: " + response.body)
+
+      val accessToken = response.body.toString.split("&")(0).split("=")(1)
+      Logger.info("Access token: " + accessToken)
+
+      response.body.toString
+    }
+  }
+}
