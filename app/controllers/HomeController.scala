40a9953a9d016a51181d35c1cb96d00fd68e3000
vincent178 <vincent.007.cn@gmail.com>
2015-11-12 07:46:43 +0800

    content-type and redirect

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,7 +4,24 @@ import play.api.mvc._
 
 object HomeController extends Controller {
 
-  def index = Action { implicit request =>
-    Ok(views.html.home.index())
+  def index(flag: String) = Action { implicit request =>
+
+    /*
+      NodeSeq then content-type is text/xml
+      JsValue then content-type is application/json
+     */
+
+
+    flag match {
+      case "json" =>
+        val json = """{"status": "success"}"""
+        Ok(json).withHeaders(CONTENT_TYPE -> "application/json")
+      case "redirect" =>
+        Status(302).withHeaders(LOCATION -> routes.ProductsController.list().toString)
+      case "html" =>
+        Ok(views.html.home.index())
+      case _ =>
+        Ok(views.html.home.index())
+    }
   }
 }
