89591f02d8e4fb752261b36e1ba3807ec1b4cc55
vincent178 <vincent.007.cn@gmail.com>
2015-11-19 17:47:39 +0800

    redis session storage

--- app/libs/redis/store/RedisSessionStore.scala
+++ app/libs/redis/store/RedisSessionStore.scala
@@ -1,5 +1,11 @@
 package libs.redis.store
 
+import redis.RedisClient
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object RedisSessionStore {
 
   val SESSION_KEY = ""
@@ -10,7 +16,17 @@ object RedisSessionStore {
   }
 
   def loadSessionFromRedis(sId: String) = {
+        implicit val akkaSystem = akka.actor.ActorSystem()
+        val redis = RedisClient()
 
+        val futurePong = redis.ping()
+        println("Ping sent!")
+        futurePong.map( pong => {
+          println(s"Redis replied with a $pong")
+        })
+
+        Await.result(futurePong, 5.seconds)
+        akkaSystem.shutdown()
   }
 
   def decode(data: String): Map[String, String] = {
