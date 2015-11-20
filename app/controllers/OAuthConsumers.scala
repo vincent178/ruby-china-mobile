df4ec763e8154d16b766d15d9c3a4d60c1d7ae9b
vincent178 <vincent.007.cn@gmail.com>
2015-11-20 22:24:49 +0800

    successfully retrieve github user

--- app/controllers/OAuthConsumers.scala
+++ app/controllers/OAuthConsumers.scala
@@ -1,17 +1,26 @@
 package controllers
 
+import play.api.libs.ws.{WSResponse, WS}
 import play.api.mvc._
 import libs.oauth2.provider.GithubAuthenticator
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import play.api.Play.current
 
 object OAuthConsumers extends Controller {
 
   def callback(provider: String) = Action.async { implicit request =>
 
     val code = request.getQueryString("code").getOrElse("")
-    GithubAuthenticator.retrieveAccessToken(code).map { accessToken =>
+    val oauthUser: Future[WSResponse] = for {
+      accessToken <- GithubAuthenticator.retrieveAccessToken(code)
+      response <- WS.url("https://api.github.com/user").withHeaders("Authorization" -> s"token ${accessToken}").get()
+    } yield response
 
-      Ok(accessToken)
+    oauthUser.map { response =>
+
+      val login = (response.json \ "login").as[String]
+      Redirect(routes.HomeController.index()).withSession("username" -> login)
     }
   }
 }
