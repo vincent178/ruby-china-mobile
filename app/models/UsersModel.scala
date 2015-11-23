7c131b9ab4bdc7ded1ddf3fc558abe6abe522a28
vincent178 <vincent.007.cn@gmail.com>
2015-11-24 07:21:31 +0800

    new user model with dependency injection

--- app/models/UsersModel.scala
+++ app/models/UsersModel.scala
@@ -1,30 +1,42 @@
-//package models
-//
-//import models.database.{UsersDatabase, UserEntity}
-//import models.database.UsersDatabase.users
-//import play.api.Logger
-//import play.api.db.slick._
-//import play.api.Play.current
-//
-//
-//object UsersModel {
-//
-//  def create(newUser: UserEntity) = {
-//    DB.withSession { implicit session =>
-//
-//      users.filter(_.username === newUser.username)
-//
-//      val exists = (for (u <- users if u.username === newUser.username) yield u).exists
-//      val insert = UserEntity.unapply(newUser).get <> (UserEntity.apply _ tupled, UserEntity.unapply)
-//      for (u <- Query(insert) if !exists) yield u
-//    }
-//  }
-//
-//  def update(newUser: UserEntity) = {
-//    DB.withSession { implicit session =>
-//      val query = for { u <- users if u.username === newUser.username} yield u
-//      query.update(newUser)
-//    }
-//  }
-//
-//}
+package models
+
+import java.sql.Timestamp
+import javax.inject.{ Inject, Singleton}
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+
+import scala.concurrent.{Future, ExecutionContext}
+
+case class User(id: Option[Int] = None,
+                email: String,
+                username: Option[String] = None,
+                avatarUrl: Option[String] = None,
+                createdAt: Option[Timestamp] = None,
+                updatedAt: Option[Timestamp] = None)
+
+
+@Singleton
+class UsersModel @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import driver.api._
+
+  private class UserTable(tag: Tag) extends Table[User](tag, "users") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def email = column[String]("email" )
+    def username = column[String]("username")
+    def avatarUrl = column[String]("avatar_url")
+    def createdAt = column[Timestamp]("created_at")
+    def updatedAt = column[Timestamp]("updated_at")
+
+    def * = (id.?, email, username.?, avatarUrl.?, createdAt.?, updatedAt.?) <> (User.tupled, User.unapply)
+  }
+
+  private val users = TableQuery[UserTable]
+
+  def list(): Future[Seq[User]] = db.run {
+    users.result
+  }
+}
