b9f417d9cf16b64c815c51050386a94b455c5739
vincent178 <vincent.007.cn@gmail.com>
2015-11-24 07:05:57 +0800

    new home page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,13 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
 
-object HomeController extends Controller {
+class HomeController extends Controller {
 
-  def index = Action { implicit request =>
-
+  def index = Action {
     Ok(views.html.home.index())
   }
 }
+
+
