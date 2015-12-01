b58be80ede9fdda96d9f13159b71946ae428042b
vincent178 <vincent.007.cn@gmail.com>
2015-12-01 15:31:26 +0800

    clean up code and add update.md

--- UPDATE.md
+++ UPDATE.md
@@ -1,3 +1,7 @@
 
 ##### 2015-11-30
- User can login via Github, save user and save login info
+  User can login via Github, save user and save login info
+
+##### 2015-12-1
+  Refactor user authentication using ActionBuilder
+
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,17 @@
 package controllers
 
-import actionbuilders.AuthenticationBuilder
 import com.google.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+import play.api.mvc._
+
+import actionbuilders.AuthenticationBuilder
 import tables.UserModel
 
-import scala.concurrent.ExecutionContext
+
 
 @Singleton
-class HomeController @Inject()(userModel: UserModel)(implicit ex: ExecutionContext) extends ApplicationController with AuthenticationBuilder {
+class HomeController @Inject()(userModel: UserModel)(implicit ex: ExecutionContext) extends Controller with AuthenticationBuilder {
+
 
   def index = AuthenticatedAction(userModel)(ex) { implicit request =>
     Ok(views.html.home.index(request.user))
