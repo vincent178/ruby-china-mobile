10106b32997fa0d5529c02268850a741b3d34721
vincent178 <vincent.007.cn@gmail.com>
2015-12-12 19:29:41 +0800

    javascript array tutorial

---
+++ tutorials/array.js
@@ -0,0 +1,42 @@
+
+var fruits = ["Apple", "Orange", "Donkey"];
+
+// pop() && push()
+// modify array itself in the end of an array
+fruits.pop();
+
+var styles = ["Jazz", "Blues"];
+styles.push("Rock'n'Roll");
+styles[styles.length-2] = "Classic";
+console.log(styles.pop());
+
+// shift() && unshift()
+var fruits = ["Apple", "Orange"];
+var apple = fruits.shift();
+fruits.unshift("Lemon");
+console.log(fruits);
+
+function arrayRandomItem(arr) {
+  return arr[Math.random()*(arr.length)];
+}
+
+// iterating
+var arr = [ "test", 2, 1.5, false ];
+
+function find(arr, value) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(find(arr, "test") === 0);
+console.log(find(arr, 2) === 1);
+console.log(find(arr, 1.5) === 2);
+console.log(find(arr, 0) === -1);
+
+function filterNumeric(arr) {
+
+}
