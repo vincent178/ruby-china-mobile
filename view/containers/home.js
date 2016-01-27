0c336418911e61c97356be4eefa76a7d44d6d84b
vincent178 <vincent.007.cn@gmail.com>
2016-01-27 22:31:31 +0800

    frontend and backend webpack

---
+++ view/containers/home.js
@@ -0,0 +1,12 @@
+//import React from 'react';
+//
+//export default class Home extends React.Component {
+//
+//  render() {
+//    return (
+//      <div className="home">
+//        Hello, {this.props.name}
+//      </div>
+//    )
+//  }
+//}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
 
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+var web = {
   entry: {
     view: './view/app.js',
     //logic: './logic/index.js'
@@ -15,5 +27,33 @@ module.exports = {
       { test: /\.css$/, loader: "style!css" },
       { test: /\.js$/, loader: "babel-loader" }
     ]
-  }
+  },
+
+  target: 'web'
 };
+
+var node = {
+  entry: {
+    //view: './view/app.js',
+    logic: './logic/index.js'
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname
+  },
+
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.js$/, loader: "babel-loader" },
+      { test: /\.json$/, loader: "json-loader"}
+    ]
+  },
+
+  target: 'node',
+  externals: nodeModules
+};
+
+module.exports = [web, node]
+;
