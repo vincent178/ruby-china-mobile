2ecfb8c341867bb32adb5facd80f182f58f2c31d
Vincent Huang <vincent.huang@sap.com>
2016-01-27 16:36:01 +0800

    use ejs instead of jade

--- src/logic/routers/index.js
+++ src/logic/routers/index.js
@@ -4,7 +4,7 @@ import home from './home';
 
 const app = express();
 
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.set('views', process.cwd() + '/view');
 
 app.use('/', home);
