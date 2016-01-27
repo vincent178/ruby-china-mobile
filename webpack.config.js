f1d921654ddba8462255386d03c42c03333a84fa
vincent178 <vincent.007.cn@gmail.com>
2016-01-27 21:53:51 +0800

    redux structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  entry: '',
-  output: {
-    path: __dirname,
-    filename: "bundle.js"
+
+  entry: './app.js',
+
+  entry: {
+    view: './view/app.js',
+    logic: './logic/index.js'
   },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname
+  },
+
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.js$/, loader: "babel-loader" }
     ]
   }
-}
+};
