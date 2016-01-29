966d746c26ad333972c4571abd39b7646a20a347
Vincent Huang <vincent.huang@sap.com>
2016-01-29 16:54:02 +0800

    update

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   var element = document.createElement('h1');
 
-  element.innerHTML = 'Hello world';
+  element.innerHTML = 'Hello world111';
 
   return element;
 };
---
+++ make-webpack-config.js
@@ -0,0 +1,2 @@
+var path = require('path');
+var webpack = require('webpack');
