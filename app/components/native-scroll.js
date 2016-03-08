1ed5647b7eed4bae3848d9e742f76c0f1d71b433
vincent178 <vincent.007.cn@gmail.com>
2016-03-08 14:39:37 +0800

    use touch event to handle pull to refresh pan gesture

--- app/components/native-scroll.js
+++ app/components/native-scroll.js
@@ -1,6 +1,5 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
-import Hammer from 'hammerjs';
 
 var resistance = 2.5;
 
@@ -8,51 +7,44 @@ export default class NativeScroll extends Component {
   constructor(props) {
     super(props);
 
-    this._panStart = this._panStart.bind(this);
-    this._panUp = this._panUp.bind(this);
-    this._panDown = this._panDown.bind(this);
-    this._panEnd = this._panEnd.bind(this);
 
-
-    this.onScroll = this.onScroll.bind(this);
+    // make bindings
+    //this.onScroll = this.onScroll.bind(this);
     this.handleTouchStart = this.handleTouchStart.bind(this);
     this.handleTouchMove = this.handleTouchMove.bind(this);
     this.handleTouchEnd = this.handleTouchEnd.bind(this);
+
+
+    this.state = {
+      pullDistance: 0
+    }
   }
 
   componentDidMount() {
-    var content = document.getElementById('content');
-    this.hm = new Hammer.Manager(content, {
-      recognizers: [
-        [Hammer.Pan, {direction: Hammer.DIRECTION_HORIZONTAL}]
-      ]
-    });
-
-    this.hm.on('panstart', this._panStart);
-    this.hm.on('panup', this._panUp);
-    this.hm.on('pandown', this._panDown);
-    this.hm.on('panend', this._panEnd);
-
-    this.pan = {
-      enabled: false,
-      distance: 0,
-      startingPositionY: 0
-    };
+    const scroll = document.getElementById('gt-scroll-content');
 
     //el.addEventListener('scroll', this.onScroll, false);
-    //el.addEventListener('touchstart', this.handleTouchStart, false);
-    //el.addEventListener('touchmove', this.handleTouchMove, false);
-    //el.addEventListener('touchend', this.handleTouchEnd, false);
+    scroll.addEventListener('touchstart', this.handleTouchStart, false);
+    scroll.addEventListener('touchmove', this.handleTouchMove, false);
+    scroll.addEventListener('touchend', this.handleTouchEnd, false);
+
+    this._startingPositionY = 0;
+    this._enablePullToRefresh = false;
+    this._start = 0;
   }
 
   componentWillUnmount() {
-    const el = ReactDOM.findDOMNode(this.refs.scroll);
-    this.hm.destroy();
+    const scroll = document.getElementById('gt-scroll-content');
 
-    el.removeEventListener('scroll', this.onScroll, false);
-    el.removeEventListener('touchstart', this.handleTouchStart, false);
-    el.removeEventListener('touchmove', this.handleTouchMove, false);
-    el.removeEventListener('touchend', this.handleTouchEnd, false);
+    scroll.removeEventListener('scroll', this.onScroll, false);
+    scroll.removeEventListener('touchstart', this.handleTouchStart, false);
+    scroll.removeEventListener('touchmove', this.handleTouchMove, false);
+    scroll.removeEventListener('touchend', this.handleTouchEnd, false);
+
+    // destroy instance data
+    this._startingPositionY = null;
+    this._enablePullToRefresh = null;
+    this._start = null;
   }
 
   onScroll() {
@@ -62,70 +54,57 @@ export default class NativeScroll extends Component {
     }
   }
 
-  _panStart() {
-    console.log("[NativeScroll] _panStart()");
-    this.pan.startingPositionY = document.body.scrollTop;
 
-    if (this.pan.startingPositionY === 0) {
-      this.pan.enabled = true;
+  handleTouchStart(e) {
+    console.log("[NativeScroll] handleTouchStart");
+
+    this._startingPositionY = document.body.scrollTop;
+    if (this._startingPositionY === 0) {
+      this._enablePullToRefresh = true;
     }
-  }
 
-  _panUp(e) {
-    console.log("[NativeScroll] _panUp() ");
-
-    if (!this.pan.enabled || this.pan.distance === 0) {
-      return;
-    }
+    const touchObj = e.changedTouches[0];
+    this._start = parseInt(touchObj.clientY, 10);
 
     e.preventDefault();
-    this.pan.distance = e.distance / resistance;
-
   }
 
-  _panDown(e) {
-    console.log("[NativeScroll] _panDown() ");
-
-    if (!this.pan.enabled) {
-      return;
-    }
-
-    var content = document.getElementById('content');
-    var ptr = document.getElementById('ptr');
-
-    e.preventDefault();
-    this.pan.distance = e.distance / resistance;
-
-    ptr.style.transform = ptr.style.webkitTransform = `translate(${this.pan.distance}px)`;
-    content.style.transform = content.style.webkitTransform = `translate(${1}px)`;
-
-  }
-
-  _panEnd() {
-    console.log("[NativeScroll] _panEnd() ");
-
-  }
-
-  handleTouchStart(evt) {
-    console.log("handleTouchStart");
-  }
-
-  handleTouchMove(evt) {
+  handleTouchMove(e) {
     console.log("handleTouchMove");
-    const el = ReactDOM.findDOMNode(this.refs.scroll);
-    if (window.scrollY >= (el.scrollHeight - window.innerHeight - 200)) {
-      this.props.dispatch(this.props.scrollFunc());
+
+    if (this._enablePullToRefresh) {
+      const touchObj = e.changedTouches[0];
+      const distance = parseInt(touchObj.clientY, 10) - this.startY;
+      if (distance > 0) {
+        this.setState({pullDistance: distance / resistance});
+      }
     }
-    if (window.scrollY < 0) {
-      debugger;
-      var ptr = document.getElementById('ptr');
-      ptr.style.transform = `translateY(0, 50px)`;
-    }
+
+
+    //const el = ReactDOM.findDOMNode(this.refs.scroll);
+    //if (window.scrollY >= (el.scrollHeight - window.innerHeight - 200)) {
+    //  this.props.dispatch(this.props.scrollFunc());
+    //}
+    //
+    //if (window.scrollY < 0) {
+    //  var ptr = document.getElementById('ptr');
+    //  ptr.style.transform = `translateY(0, 50px)`;
+    //}
   }
 
   handleTouchEnd(evt) {
     console.log("handleTouchEnd");
 
+    if (this._enablePullToRefresh) {
+      this._enablePullToRefresh = false;
+      this.setState({pullDistance: 0});
+    }
+  }
+
+  translateCss() {
+    return {
+      transform: `${this.state.pan}px`
+    }
   }
 
   render() {
@@ -140,7 +119,7 @@ export default class NativeScroll extends Component {
           </div>
         </div>
 
-        <div id="content" className="scroll-container">
+        <div id="gt-scroll-content">
           {this.props.children}
         </div>
       </div>
