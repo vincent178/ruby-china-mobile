b7ca948f0b35457145e606ffc54262ca7a90d1ab
vincent178 <vincent.007.cn@gmail.com>
2016-07-26 11:49:13 +0800

    add font-awesome

--- server/ssr/page.js
+++ server/ssr/page.js
@@ -12,6 +12,7 @@ const createPage = (content = '', state = {}, options) => {
         <meta charset="UTF-8"/>
         <title>Ruby China Mobile</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
       </head>
       <body>
         <div id="app">${content}</div>
