29a788a929359e7dcc38b9431b80e4f76318fd2c
vincent178 <vincent.007.cn@gmail.com>
2016-07-29 08:17:08 +0800

    add devtool and hot reload support

--- common/store/index.js
+++ common/store/index.js
@@ -1,10 +1,43 @@
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore as _createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers';
 
+const hasReduxDevTool = () => {
+  return process.browser
+    && window.DEV !== 'production'
+    && window.devToolsExtension;
+};
+
 export default (initialState) => {
+  const create = hasReduxDevTool()
+    ? window.devToolsExtension()(_createStore)
+    : _createStore;
+
+  const createStore = applyMiddleware(...middlewares)(create);
+  const store = createStore(rootReducer, initialState);
+
+  module.hot && module.hot.accept('../reducers', () => {
+    store.replaceReducer(require('../reducers'));
+  });
+
+  return store;
+};
+
+export default (initialState) => {
+
+  const create = hasReduxDevTool()
+    ? window.devToolsExtension()(_createStore)
+    : _createStore;
+
+
   const middlewares = [ thunk ];
-  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-  return createStoreWithMiddleware(reducers, initialState);
+  const createStore = applyMiddleware(...middlewares)(create);
+  const store = createStore(reducers, initialState);
+
+  module.hot && module.hot.accept('../reducers', () => {
+    store.replaceReducer(require('../reducers'));
+  });
+
+  return store;
 };
 
