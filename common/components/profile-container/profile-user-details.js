6592ac3f7aa70b725845ae1ed8ae5707426b27c3
vincent178 <vincent.007.cn@gmail.com>
2016-08-17 13:00:34 +0800

    完成用户详细信息页面

--- common/components/profile-container/profile-user-details.js
+++ common/components/profile-container/profile-user-details.js
@@ -13,17 +13,24 @@ export default class ProfileUserDetails extends Component {
 
     const { user } = this.props;
 
+    let userInfoArray = [];
     let userCompanyLocation = "";
 
-    if (user.company && user.company.length === 0) {
-      userCompanyLocation = user.location;
-    } else if (user.location && user.location.length === 0) {
-      userCompanyLocation = user.company;
-    } else {
-      userCompanyLocation = `${user.company}@${user.location}`;
+    if (user.company && user.company.length > 0) {
+      userInfoArray.push(user.company);
     }
 
-    console.log(user);
+    if (user.location && user.location.length > 0) {
+      userInfoArray.push(user.location);
+    }
+
+    if (userInfoArray.length === 2) {
+      userCompanyLocation = userInfoArray.join("@");
+    }
+
+    if (userInfoArray.length === 1) {
+      userCompanyLocation = userInfoArray[0];
+    }
 
     return (
       <div className={styles.profileUserDetailsContainer}>
@@ -37,8 +44,7 @@ export default class ProfileUserDetails extends Component {
           <div className={styles.profileFullname}>{`${user.name}`}</div>
           <div className={styles.profileUsername}>{`${user.login}`}</div>
           <div>{userCompanyLocation}</div>
-          <div>{user.created_at.slice(0, 10)}加入</div>
-          <div>twitter/website/github/email</div>
+          <div>{user.created_at.slice(0, 10)} 加入</div>
         </div>
       </div>
     );
