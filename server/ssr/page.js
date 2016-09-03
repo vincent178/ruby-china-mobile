d39b78a600638e7b85c51c654991f0faaf14fb19
vincent178 <vincent.007.cn@gmail.com>
2016-09-03 17:19:15 +0800

    更新，使用合适的viewport

--- server/ssr/page.js
+++ server/ssr/page.js
@@ -27,10 +27,9 @@ const createPage = (content = '', state = {}) => {
       <head>
         <meta charset="utf-8" />
         <title>Ruby China Mobile</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
         <link rel="stylesheet" href="http://libs.useso.com/js/font-awesome/4.2.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="/${assets.client.css}">
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"> -->
       </head>
       <body>
         <div id="app">${content}</div>
